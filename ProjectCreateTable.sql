6adb7149ebcfcccd18c7380e59ea1c0dc0159e3e
1074Perk <pxp210041@utdallas.edu>
2024-10-15 21:55:13 -0500

    Update ProjectCreateTable.sql
    Added foreign key restraints

--- ProjectCreateTable.sql
+++ ProjectCreateTable.sql
@@ -1,37 +1,45 @@
 CREATE TABLE Company (
-	name CHAR(20) PRIMARY KEY,
+	name CHAR(50) PRIMARY KEY,
 	founding_yr INT,
-	parent_company CHAR(20)
+	parent_company CHAR(50),
+	FOREIGN KEY (parent_company) REFERENCES Company(name)
 );
 
 CREATE TABLE Game (
-	developer CHAR(20),
-	publisher CHAR(20),
-	gm_id CHAR(20) PRIMARY KEY,
-	name CHAR(20),
-	genre CHAR(20)
+	developer CHAR(50),
+	publisher CHAR(50),
+	gm_id INT PRIMARY KEY,
+	name CHAR(50),
+	FOREIGN KEY (developer) REFERENCES Company(name),
+	FOREIGN KEY (publisher) REFERENCES Company(name)
 );
 
+CREATE TABLE Genres (
+	genreID INT PRIMARY KEY,
+	gameID INT,
+	FOREIGN KEY (gameID) REFERENCES Game(gm_id)
+);
 
 CREATE TABLE Series (
-	name CHAR(20),
-	games CHAR(20) PRIMARY KEY,
-	FOREIGN KEY (games) REFERENCES Game(gm_id)
-);
-
-CREATE TABLE Emulator (
-	name CHAR(20) PRIMARY KEY,
-	emulated_console CHAR(20),
-	int_rel INT,
-	license CHAR(20),
-	current_rel_ver CHAR(20),
-	website CHAR(20)
+	name CHAR(50),
+	game INT PRIMARY KEY,
+	FOREIGN KEY (game) REFERENCES Game(gm_id)
 );
 
 CREATE TABLE Console (
 	rel_yr INT,
 	eol_yr INT,
-	name CHAR(20) PRIMARY KEY,
-	console_developer CHAR(20),
-	FOREIGN KEY (name) references Company(name)
+	name CHAR(50) PRIMARY KEY,
+	console_developer CHAR(50),
+	FOREIGN KEY (console_developer) references Company(name)
+);
+
+CREATE TABLE Emulator (
+	name CHAR(50) PRIMARY KEY,
+	emulated_console CHAR(50),
+	int_rel INT,
+	license CHAR(50),
+	current_rel_ver CHAR(50),
+	website CHAR(100),
+	FOREIGN KEY (emulated_console) REFERENCES Console(name)
 );
